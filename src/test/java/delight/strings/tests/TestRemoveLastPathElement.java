1ac8544670fe41e5b0cf4940ff96001bad3700f4
mxro <mxro@nowhere.com>
2015-08-21 19:26:32 +1000

    Implementing ObjectHub JS framework Autocommit Fri Aug 21 19:26:32 EST 2015

--- src/test/java/delight/strings/tests/TestRemoveLastPathElement.java
+++ src/test/java/delight/strings/tests/TestRemoveLastPathElement.java
@@ -1,10 +1,16 @@
 package delight.strings.tests;
 
+import delight.strings.UriUtils;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class TestRemoveLastPathElement {
 
     @Test
     public void test() {
-
+        Assert.assertEquals("my", UriUtils.removeLastElement("my/path"));
     }
 
 }
