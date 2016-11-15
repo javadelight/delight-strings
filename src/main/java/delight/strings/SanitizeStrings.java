60565716370426128dcdef5aada95e38e73f813f
mxro <mxro@nowhere.com>
2016-11-16 07:56:53 +1100

    Working on ObjectHub and Stellar DB Autocommit Wed Nov 16 07:56:53 AEDT 2016

--- src/main/java/delight/strings/SanitizeStrings.java
+++ src/main/java/delight/strings/SanitizeStrings.java
@@ -7,7 +7,8 @@ public final class SanitizeStrings {
             '7', '8', '9', '0', '.', '+', '$' };
 
     public static final char[] BASIC_CHARACTERS = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_' };
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', '0' };
 
     /**
      * Returns true if the given character is valid in an URL path portion
